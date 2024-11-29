408890c3ee7e1c2f1203384003a396934686d935
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-29 20:15:40 +0600

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <header className="bg-blue-500 text-white p-4 text-center">
+    <h1 className="text-3xl font-bold">Dork Generator</h1>
+  </header>
+);
+
+export default Header;
