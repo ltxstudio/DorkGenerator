03ad3a48cc2d01d7cd91c3d140bebdaed7ee0a95
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-29 20:16:40 +0600

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-gray-800 text-white p-4 text-center">
+    <p>&copy; {new Date().getFullYear()} Dork Generator. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
